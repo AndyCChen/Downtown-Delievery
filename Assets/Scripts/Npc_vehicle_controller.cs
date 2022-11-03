f043bc10bad191be4d2dc3edc42871b86c2c7c98
Andy Chen <acchen@cpp.edu>
2022-11-03 00:10:33 -0700

    made vehicle only move if at least on wheel is grounded

--- Assets/Scripts/Npc_vehicle_controller.cs
+++ Assets/Scripts/Npc_vehicle_controller.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Npc_vehicle_controller : MonoBehaviour
 {
+    [SerializeField] private Rigidbody vehicleObject;
+    [SerializeField] private WheelCollider front_left_wheel;
+    [SerializeField] private WheelCollider front_right_wheel;
+    [SerializeField] private WheelCollider back_left_wheel;
+    [SerializeField] private WheelCollider back_right_wheel;
+
     public float force;
-    public Rigidbody vehicleObject;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +21,8 @@ public class Npc_vehicle_controller : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetAxisRaw("Vertical") > 0)
+        // only be able to move if at least one wheel is touching the ground
+        if (Input.GetAxisRaw("Vertical") > 0  && (front_left_wheel.isGrounded || front_right_wheel.isGrounded || back_left_wheel.isGrounded || back_right_wheel.isGrounded))
         {
             vehicleObject.AddRelativeForce(Vector3.forward * force);
         }
