66b236fc6809b4c4f72299d6f4bce7d5fc8c5dda
Andy Chen <acchen@cpp.edu>
2022-11-04 14:35:45 -0700

    implement movement and steering for wheel coliders, added center of mass, friction values may need adjusting

--- Assets/Scripts/Npc_vehicle_controller.cs
+++ Assets/Scripts/Npc_vehicle_controller.cs
@@ -2,21 +2,57 @@ using UnityEngine;
 
 public class Npc_vehicle_controller : MonoBehaviour
 {
-    [SerializeField] private Rigidbody vehicleObject;
     [SerializeField] private WheelCollider front_left_wheel;
     [SerializeField] private WheelCollider front_right_wheel;
     [SerializeField] private WheelCollider back_left_wheel;
     [SerializeField] private WheelCollider back_right_wheel;
 
-    public float force;
+    public Rigidbody vehicle_rb;
+    public GameObject centerOfMassPosition;
+
+    public float acceleration;
+    public float breakingForce;
+    public float maxTurnAngle;
+
+    private float currentAcceleration;
+    private float currentBreakingForce;
+    private float currentTurnAngle;
+
+    private void Start()
+    {
+        vehicle_rb.GetComponent<Rigidbody>().centerOfMass = centerOfMassPosition.transform.localPosition;
+        Debug.Log(centerOfMassPosition.transform.localPosition);
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        // only be able to move if at least one wheel is touching the ground
-        if (Input.GetAxisRaw("Vertical") > 0  && (front_left_wheel.isGrounded || front_right_wheel.isGrounded || back_left_wheel.isGrounded || back_right_wheel.isGrounded))
+        currentAcceleration = acceleration * Input.GetAxis("Vertical");
+        Debug.Log(currentAcceleration);
+
+        // apply break force if spacebar is held
+        if (Input.GetKey(KeyCode.Space))
         {
-            vehicleObject.AddRelativeForce(Vector3.forward * force);
+            currentBreakingForce = breakingForce;
+
+        } else
+        {
+            currentBreakingForce = 0f;
         }
+
+        // apply acceleration to front wheels
+        front_left_wheel.motorTorque = currentAcceleration;
+        front_right_wheel.motorTorque = currentAcceleration;
+
+        // apply current break force to all wheels
+        front_left_wheel.brakeTorque = currentBreakingForce;
+        front_right_wheel.brakeTorque = currentBreakingForce;
+        back_left_wheel.brakeTorque = currentBreakingForce;
+        back_right_wheel.brakeTorque = currentBreakingForce;
+
+        // set current turning angle
+        currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+        front_left_wheel.steerAngle = currentTurnAngle;
+        front_right_wheel.steerAngle = currentTurnAngle;
     }
 }
