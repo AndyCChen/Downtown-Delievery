e4dafc06f25f3da5c2fb1da6429fc3410ae507c8
Andy Chen <acchen@cpp.edu>
2022-12-11 16:10:29 -0800

    directional arrow now rotated smoothly

--- Assets/Scripts/DirectionalArrow.cs
+++ Assets/Scripts/DirectionalArrow.cs
@@ -1,16 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DirectionalArrow : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    private Transform arrowTransform;
+    public float rotationSpeed;
+
+    private void Start()
+    {
+        arrowTransform = transform.GetChild(0);
+    }
 
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
-        Vector3 targetPosition = target.transform.position;
-        targetPosition.y = transform.position.y;
-        transform.LookAt(target);
+        Vector3 targetDirection = target.position - arrowTransform.position;
+
+        // only rotate around y axis
+        targetDirection.y = 0.0f;
+
+        arrowTransform.rotation = Quaternion.RotateTowards(arrowTransform.rotation, Quaternion.LookRotation(targetDirection), rotationSpeed * Time.deltaTime);
+    }
+
+    public void SetTarget(Transform rotationTarget)
+    {
+        target = rotationTarget;
     }
 }
